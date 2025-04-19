ce20e80c73b4c9af3a7d33901afc04179b3a559a
SomeAsianGuy14 <35354097+SomeAsianGuy14@users.noreply.github.com>
2025-04-19 19:05:42 -0400

    Update graph.py
    get functions and dfs

--- graph.py
+++ graph.py
@@ -1,9 +1,18 @@
 class Graph:
-        def __init__(self):
+    def __init__(self):
         self.users = {} #name ,id
         self.friends = [] #list of friends
         self.destination = [] #list of destinations
         self.date = [] #list of dates
+
+    def getFriends(self, user) -> list:
+        return self.friends[self.users[user]]
+
+    def getDestination(self, user):
+        return self.destination[self.users[user]]
+
+    def getDate(self, user):
+        return self.date[self.users[user]]
 
     def insert(self, name, friends, destination, date):
         if name not in self.users: #check if user is already in system
@@ -32,15 +41,34 @@ class Graph:
     def bfs(self, dest, date, user) -> list:
         #destination string size int user string
         q = []
-        visited = []
+        visited = set()
         out = []
         q.append(self.users[user])
-        visited.append(self.users[user])
-        while len(q) > 0:
-            cur = q.popleft()
-            for friend in self.friends[cur]:
-                if friend not in visited and friend not in out:
-                    q.append(friend)
-            if self.destination[cur] == dest and self.date[cur] == date:
-                out.append(cur)
+        visited.add(self.users[user])
+
+        while q:
+            cur = q.pop()
+            if cur not in visited:
+                if self.destination[cur] == dest and self.date[cur] == date:
+                    out.append(cur)
+                for friend in self.friends[cur]:
+                    if friend not in visited and friend not in out:
+                        q.append(friend)
+        return out
+
+    def dfs(self, dest, date, user) -> list:
+        # destination: string, date: int, user: string
+        visited = set()
+        out = []
+        stack = [self.users[user]]
+
+        while stack:
+            cur = stack.pop()
+            if cur not in visited:
+                visited.add(cur)
+                if self.destination[cur] == dest and self.date[cur] == date:
+                    out.append(cur)
+                for friend in reversed(self.friends[cur]): #mimic stack behavior
+                    if friend not in visited and friend not in out:
+                        stack.append(friend)
         return out
