b725cfbb7d08050623dea3a478b929856ed7a927
Natalia Rojas <natalia.rojas@ufl.edu>
2025-04-19 20:38:05 -0400

    added comments on grpah.py, implemented program functionality

--- build.py
+++ build.py
@@ -1,46 +1,117 @@
 import pandas as pd
 from graph import Graph
-# To run on terminal: 
-# pip install pandas 
-# pip install openpyxl
-# python main.py 
 
+# this doesn't work.
+def displayMenu(username):
+    print("---------------------")
+    print("\nHello " + username)
+    print("\nMenu: \n")
+    print("1. View Account \n2. Search Friends \n3. Log Out\n")
+    print("---------------------")
+    choice = input("\nPlease select a choice from the menu: ")
 
+    if choice == 1:
+        # Display User ID, User Name, Destination, Travel Date
+        # display main menu
+        print("Profile:\n")
+        print("User ID: ")
+        print("User Name: ")
+        print("Destination: ")
+        print("Travel Date: \n")
+        displayMenu(username)
 
-# Replace 'your_file.xlsx' with the path to your Excel file
-file_path = 'dataset_group.xlsx'
+    elif choice == 2:
+        # display search results 
+        print("Search results:")
 
-# Read the Excel file into a DataFrame
-df = pd.read_excel(file_path)
+    elif choice == 3:
+        username = login()
 
-# Display the first few rows of the dataframe
-# To run: type python main.py on terminal.
+    else:
+        print("\nPlease select a valid option.")
+        choice = displayMenu(username)
+
+def login():
+    print("Welcome to GatorGo!")
+    username = input("\nPlease enter your username: ")
+    while g.searchUserName(username) == False:
+        print("\nCould not find user, please try again.\n")
+        g.tempInsert(username) 
+        username = input("Please enter your username: ")
+    return username
+
+'''
+Instructions to run on terminal: 
+
+pip install pandas
+pip install openpyxl
+python build.py 
+'''
+
+# dataset format: 
+# User ID (int)    User Name (str)    Destination (int)     Friends (list of str)     Date of Travel (int)
+
+'''
+Instructions for reading the file
+
+Display the first few rows of the dataframe
 print(df.head())
 
-# Access a specific column
-column_data = df['User ID']  # Replace 'ColumnName' with the actual column name
-print(column_data)
+Access a specific column
+column_data = df['column header']
 
-# Access a specific row by index
-row_data = df.iloc[0]  # Access the first row
-print(row_data)
-
-'''
-create the graph here with dataset info
+Access a specific row (by index)
+row_data = df.iloc[index]         
 '''
 
-users = Graph()
-output = users.display()
 
-expectedOutput = [
-    "A -> ['B', 'C']",
-    "B -> ['D']",
-    "C -> []",
-    "D -> []",
-    "E -> []]"
-]
+# SETUP FOR READING THE FILE
 
-if(output == expectedOutput):
-    print("true")
-else:
-    print("false")
+# Set the file path and read into DataFrame (df)
+file_path = 'dataset_group.xlsx'
+df = pd.read_excel(file_path)
+
+print(df.head()) # for TESTING
+
+# Get number of user (number of userIDS)
+userIDS = df['User ID']  
+print("Number of users: " + str(userIDS.size)) # for TESTING
+
+# FOR TESTING
+print("first 5 users:")
+for i in range (5):
+    userId = df.iloc[i, 0]
+    userName = df.iloc[i, 1]
+    print("USER ID: " + str(userId) + " USER NAME: " + userName)
+
+# CREATING THE GRAPH FROM FILE DATA
+g = Graph()
+# for each user in file, insert
+#                        set userID, userName, destination, friends, travelDate
+
+# when setting userID, append 0s at the beginning so they're all the same size (6 digits) 
+print()
+
+# PROGRAM RUN
+
+username = login()
+displayMenu(username)
+
+# if choice == 1:
+#     # Display User ID, User Name, Destination, Travel Date
+#     # display main menu
+#     print("Profile:\n")
+#     print("User ID: ")
+#     print("User Name: ")
+#     print("Destination: ")
+#     print("Travel Date: \n")
+#     choice = displayMenu()
+
+# elif choice == 2:
+#     # display search results 
+#     print("Search results:")
+# elif choice == 3:
+#     username = login()
+# else:
+#     print("\nPlease select a valid option.")
+#     choice = displayMenu()
--- graph.py
+++ graph.py
@@ -1,14 +1,104 @@
 class Graph:
     def __init__(self):
-        self.adj_list = {}
+        # this won't work. Other class for User??
+        self.users = {} #name ,id 
+        self.friends = [] #list of friends 
+        self.destination = [] #list of destinations 
+        self.date = [] #list of dates 
+    
+    # insert(userID, userName, destination travelDate)
+    def insert(self, name, friends, destination, date):
+        if name not in self.users: #check if user is already in system
+            self.users[name] = len(self.users)
+            for friend in friends:
+                if friend not in self.users:
+                    self.users[friend] = len(self.users)
+                    self.friends[self.users[name]].append(self.users[friend])
+            self.friends.append(friends)
+            self.destination.append(destination)
+            self.date.append(date)
 
-    def addVertex(self, vertex):
+    # user, friend
+    def addFriend(self, name, friend) -> bool: #undirected graph
+        if name not in self.users:
+            return False
+        if friend not in self.users:
+            self.users[friend] = len(self.users)
+        self.friends[self.users[name]].append(self.users[friend]);
+        return True
+
+    def removeFriend(self, name, friend) -> bool:
+        if name not in self.users or friend not in self.users:
+            return False
+        self.friends[self.users[name]].remove(self.users[friend])
+
+    # self (unnecessary parameters dest, date, what is user??)
+    # do we need bfs/dfs?? should we compare data structure or search algorithm instead?? 
+
+    # returns list of friends by their userIDs that have same dest and similar date (+-2 days)
+    def searchFriends(self, dest, date):
         pass
 
-    def addEdge(self, v1, v2):
+    # returns number of friends that have same dest and similar date (+-2 days)
+    def searchNumFriends(self, dest, data):
         pass
 
-    def display(self):
+    # HELPER FUNCTIONS
+
+    # returns user's user name
+    def getUserName(self, user):
+        pass
+
+    # returns user's destination 
+    def getDestination(self, user):
+        pass
+    
+    # returns user's date of travel
+    def getTravelDate(self, user):
+        pass
+
+    def bfs(self, dest, date, user) -> list:
+        #destination string size int user string
+        q = []
+        visited = []
+        out = []
+        q.append(self.users[user])
+        visited.append(self.users[user])
+        while len(q) > 0:
+            cur = q.popleft()
+            for friend in self.friends[cur]:
+                if friend not in visited and friend not in out:
+                    q.append(friend)
+            if self.destination[cur] == dest and self.date[cur] == date:
+                out.append(cur)
+        return out
+    
+    def getBFSTime(self):
+        pass
+    
+    def dfs(self, dest, date, user):
+        pass
+
+    def getDFSTime(self):
+        pass
+    
+    # this won't work yet, might need to change based on restructure
+    def test(self):
         outputList = []
-        for vertex in self.adj_list:
-            outputList += (f"{vertex} -> {self.adj_list[vertex]}")
+        for user in self.friends:
+            outputList.append(f"{user} -> {' '.join(self.friends[user])}")
+        return outputList
+    
+    # FUNCTIONS FOR PROGRAM RUN
+
+    def tempInsert(self, user): #testing something, remove later
+        self.users[0] = user
+        
+    # returns true if user exists
+    def searchUserName(self, userName):
+        return userName in self.users.values()
+    
+    def searchResults(self, user):
+        # from given user, return all friends that go to the same location on similar date
+        # print the search times (DFS vs BFS). ?? unless we change this
+        pass
